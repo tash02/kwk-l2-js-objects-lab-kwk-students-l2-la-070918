30504f80f5a5fc2be6a952947133e8e405c0f2b2
tash02 <tash.brinkley@gmail.com>
2018-07-12 18:04:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 // Write your solution in this file!
- driver = {
-name: "Sam", 
-address: "11 Broadway"
-}
+ driver = {name: "Sam", address: "11 Broadway"}
 function updateDriverWithKeyAndValue (driver, key, value){
   // driver.age="15"
   return Object.assign(
-    {},driver, {key: value}
+    {},driver, {[key]: value}
     )
 }
